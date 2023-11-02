8c6934577124eaad77ef10357f83b79cc718dc66
Zhan <gian910147@gmail.com>
2023-11-02 15:41:53 +0800

    setiing for github page

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://zhanjian0217.github.io/", // replace this with your deployed domain
   author: "zhan",
   desc: "my technical articles",
   title: "Zhan's blog",
